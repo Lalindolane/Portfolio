68cbe7f1dbd363e8df3ecaeb81209cfd647abbda
Lane Lindstrom <lalindo.lane@gmail.com>
2026-05-15 15:05:54 -0600

    website

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,9 +1,8 @@
-import "./App.css";
-
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import "./styles/App.css"
 
 function App() {
   return (
