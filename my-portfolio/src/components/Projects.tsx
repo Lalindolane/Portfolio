625a8d5769133f51dd8820aa8cac43c1338fd744
Lane Lindstrom <lalindo.lane@gmail.com>
2026-07-01 10:23:50 -0600

    styling change

--- my-portfolio/src/components/Projects.tsx
+++ my-portfolio/src/components/Projects.tsx
@@ -60,7 +60,7 @@ export default function Projects() {
             id="projects"
             className="section"
         >
-            <h2 className="section-title">
+            <h2 className="section-title" style={{ marginTop: "10px" }}>
                 Other Minor Projects
             </h2>
 
